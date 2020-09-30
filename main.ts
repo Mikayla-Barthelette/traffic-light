157e18afa2afa06b152af76e2c9e379700efe650
Mikayla-Barthelette <71400623+Mikayla-Barthelette@users.noreply.github.com>
2020-09-30 13:12:05 -0400

    added colours

--- main.ts
+++ main.ts
@@ -1,3 +1,13 @@
+let range: neopixel.Strip = null
+let strip = neopixel.create(DigitalPin.P16, 24, NeoPixelMode.RGB)
 basic.forever(function () {
-	
+    range = strip.range(2, 4)
+    strip.showColor(neopixel.colors(NeoPixelColors.Green))
+    basic.pause(1000)
+    range = strip.range(1, 4)
+    strip.showColor(neopixel.colors(NeoPixelColors.Yellow))
+    basic.pause(1000)
+    range = strip.range(0, 4)
+    strip.showColor(neopixel.colors(NeoPixelColors.Red))
+    basic.pause(1000)
 })
